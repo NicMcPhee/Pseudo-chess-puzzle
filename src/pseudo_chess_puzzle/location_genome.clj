3d122807acc09febe09da767082e3fbf18f98e2d
Nic McPhee <mcphee@morris.umn.edu>
2015-02-21 17:15:49 -0500

    Added all the checks that I think will be needed to compute the errors.
    Added a bunch of predicates that I hope will, collectively, give me everything I need to compute the error vector.

--- src/pseudo_chess_puzzle/location_genome.clj
+++ src/pseudo_chess_puzzle/location_genome.clj
@@ -3,6 +3,48 @@
 (defn position-string-representation [piece]
   (reduce #(str %1 (get (name %2) 0)) "" (vals piece)))
 
+(defn share-row? [board x y]
+  (let [x-pos (.indexOf board x)
+        y-pos (.indexOf board y)]
+    (= (quot x-pos 4) (quot y-pos 4))))
+
+(defn share-col? [board x y]
+  (let [x-pos (.indexOf board x)
+        y-pos (.indexOf board y)]
+    (= (rem x-pos 4) (rem y-pos 4))))
+
+(defn share-diag? [board x y]
+  (let [x-pos (.indexOf board x)
+        y-pos (.indexOf board y)
+        diff (- x-pos y-pos)]
+    (or (zero? (rem diff 5))
+        (and (zero? (rem diff 3))
+             (= (> x-pos y-pos)
+                (< (rem x-pos 4) (rem y-pos 4)))))))
+
+(defn share-row-col-diag? [board x y]
+  (or (share-row? board x y)
+      (share-col? board x y)
+      (share-diag? board x y)))
+
+(defn adjacent? [board x y]
+  (let [x-pos (.indexOf board x)
+        y-pos (.indexOf board y)]
+    (and (>= 1 (Math/abs (- (rem x-pos 4) (rem y-pos 4))))
+         (>= 1 (Math/abs (- (quot x-pos 4) (quot y-pos 4)))))))
+
+(defn two-apart? [board x y]
+  (let [x-pos (.indexOf board x)
+        x-row (quot x-pos 4)
+        x-col (rem x-pos 4)
+        y-pos (.indexOf board y)
+        y-row (quot y-pos 4)
+        y-col (rem y-pos 4)]
+    (or (and (= x-row y-row)
+             (= 3 (Math/abs (- x-col y-col))))
+        (and (= x-col y-col)
+             (= 3 (Math/abs (- x-row y-row)))))))
+
 (def placed-pieces
   [ { :color :black :piece :B }
     { :color :white :piece :K }
--- test/pseudo_chess_puzzle/midje.clj
+++ test/pseudo_chess_puzzle/midje.clj
@@ -22,3 +22,107 @@
       (lg/position-string-representation { :color :white :piece :B }) => "wB"
       (lg/position-string-representation { :color :black :piece :K }) => "bK"
       (lg/position-string-representation { :color :white :piece :R }) => "wR")
+
+(fact "`share-row?` returns `true` exactly when two pieces share a row"
+      ; I'm going to cheat and do the tests with integer values instead of actual
+      ; pieces, as that cuts down substantially on the clutter here.
+      (let [test-board (range 16)]
+        (lg/share-row? test-board 1 9)  => false
+        (lg/share-row? test-board 7 1)  => false
+        (lg/share-row? test-board 6 8)  => false
+        (lg/share-row? test-board 11 6) => false
+        (lg/share-row? test-board 7 15) => false
+        (lg/share-row? test-board 14 1) => false
+        (lg/share-row? test-board 4 10) => false
+        (lg/share-row? test-board 11 1) => false
+        (lg/share-row? test-board 14 6) => false
+
+        (lg/share-row? test-board 2 0)   => true
+        (lg/share-row? test-board 7 4)   => true
+        (lg/share-row? test-board 8 9)   => true
+        (lg/share-row? test-board 13 15) => true
+        ))
+
+(fact "`share-col?` returns `true` exactly when two pieces share a column"
+      (let [test-board (range 16)]
+        (lg/share-col? test-board  3  9) => false
+        (lg/share-col? test-board  7  9) => false
+        (lg/share-col? test-board 11  1) => false
+        (lg/share-col? test-board 12 13) => false
+        (lg/share-col? test-board 14 15) => false
+        (lg/share-col? test-board 13 14) => false
+        (lg/share-col? test-board  6  5) => false
+        (lg/share-col? test-board 10  3) => false
+
+        (lg/share-col? test-board  0 12) => true
+        (lg/share-col? test-board  9  1) => true
+        (lg/share-col? test-board  0 12) => true
+        (lg/share-col? test-board 14 10) => true
+        ))
+
+(fact "`share-diag?` returns `true` exactly when two pieces share a (not necessarily main) diagonal"
+      (let [test-board (range 16)]
+        (lg/share-diag? test-board 13 15) => false
+        (lg/share-diag? test-board  1 10) => false
+        (lg/share-diag? test-board 11 12) => false
+        (lg/share-diag? test-board 10  3) => false
+        (lg/share-diag? test-board  2 11) => false
+        (lg/share-diag? test-board  7 14) => false
+        (lg/share-diag? test-board  0  1) => false
+        (lg/share-diag? test-board 13  9) => false
+        (lg/share-diag? test-board  1  8) => false
+
+        (lg/share-diag? test-board 10  0) => true
+        (lg/share-diag? test-board 13  8) => true
+        (lg/share-diag? test-board  4 14) => true
+        (lg/share-diag? test-board  1  6) => true
+        (lg/share-diag? test-board  2  7) => true
+        (lg/share-diag? test-board  4  1) => true
+        (lg/share-diag? test-board  5  8) => true
+        (lg/share-diag? test-board 12  6) => true
+        (lg/share-diag? test-board  7 13) => true
+        (lg/share-diag? test-board 11 14) => true
+        ))
+
+(fact "`adjacent?` returns `true` exactly when two pieces are immediately adjacent (including along a diagonal)"
+      (let [test-board (range 16)]
+        (lg/adjacent? test-board  2 11) => false
+        (lg/adjacent? test-board 10  0) => false
+        (lg/adjacent? test-board 14  8) => false
+        (lg/adjacent? test-board  2 11) => false
+        (lg/adjacent? test-board  1 11) => false
+        (lg/adjacent? test-board  9 11) => false
+        (lg/adjacent? test-board  5 14) => false
+        (lg/adjacent? test-board 12 10) => false
+        (lg/adjacent? test-board  4  3) => false
+
+        (lg/adjacent? test-board  9 10) => true
+        (lg/adjacent? test-board  9  6) => true
+        (lg/adjacent? test-board  4  5) => true
+        (lg/adjacent? test-board 15 10) => true
+        (lg/adjacent? test-board 14 11) => true
+        (lg/adjacent? test-board  5  0) => true
+        (lg/adjacent? test-board  9 12) => true
+        ))
+
+(fact "`two-apart?` returns `true` exactly when the two pieces are exactly two spaces apart, horizontally or vertically"
+      (let [test-board (range 16)]
+        (lg/two-apart? test-board  3  9) => false
+        (lg/two-apart? test-board  9  5) => false
+        (lg/two-apart? test-board  0 13) => false
+        (lg/two-apart? test-board 15  4) => false
+        (lg/two-apart? test-board 13  7) => false
+        (lg/two-apart? test-board 13 14) => false
+        (lg/two-apart? test-board  3  6) => false
+        (lg/two-apart? test-board  2 15) => false
+        (lg/two-apart? test-board  7  6) => false
+        (lg/two-apart? test-board  8  2) => false
+
+        (lg/two-apart? test-board  8 11) => true
+        (lg/two-apart? test-board 12 15) => true
+        (lg/two-apart? test-board 12  0) => true
+        (lg/two-apart? test-board 14  2) => true
+        (lg/two-apart? test-board  4  7) => true
+        (lg/two-apart? test-board  3 15) => true
+        (lg/two-apart? test-board 15 12) => true
+        ))
